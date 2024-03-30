969377bbef67c1fae22b1c0a22e16cdc16e4d491
OrhanBC <orhancangurel@gmail.com>
2024-03-30 16:18:12 -0400

    Openai

---
+++ src/app/api/openai/openai.ts
@@ -0,0 +1,14 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+});
+
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+  });
+}
+
+main();
