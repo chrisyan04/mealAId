4a962a4b27dc8099acfa56a66bfc60828dd2a62b
the creator <chenxi.yan@gmail.com>
2024-03-31 07:37:23 -0400

    auth key fix

--- src/app/api/openai/openai.ts
+++ src/app/api/openai/openai.ts
@@ -5,7 +5,7 @@ dotenv.config();
 //@ts-nocheck
 const openai = new OpenAI({
   //@ts-nocheck
-  apiKey: process.env.OPENAI_API_KEY , // This is the default and can be omitted
+  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
 });
 
 function buildPrompt(mealDict:any){
